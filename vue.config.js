2ee56a049941903f7d70f53d0c6a5d72f698d3e8
Spanri <kozlova9v@mail.ru>
2019-10-16 20:31:21 +0300

    gitlab

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   assetsDir: './',
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/westb/'
+    : '/',
   css: {
     loaderOptions: {
       sass: {
