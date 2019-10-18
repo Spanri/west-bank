fe0a7f0acb808ff748929d52380b62c2b1cdf115
Spanri <kozlova9v@mail.ru>
2019-10-18 21:51:29 +0300

    fix design

---
+++ src/icons/ProfilePhoto.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    width="100" height="100"
+    viewBox="0 0 100 100" fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+    d="M64.0887 29.8803C64.0887 39.1267 57.5943 46.2606 49.9999 46.2606C42.4055 46.2606 35.9112 39.1267 35.9112 29.8803C35.9112 26.878 36.6061 24.0745 37.8104 21.6666C40.2914 16.7064 44.8754 13.5 49.9999 13.5C55.1245 13.5 59.7085 16.7064 62.1895 21.6666C63.3938 24.0745 64.0887 26.878 64.0887 29.8803ZM50 54.3693C52.1678 54.3693 54.6975 53.8026 56.9304 52.8385C58.8425 52.0131 60.7166 50.8179 61.9286 49.2606H68.7659C70.6509 49.2606 72.2067 50.637 72.434 52.4248L74.4711 68.4469C74.7412 70.5713 73.0539 72.5 70.803 72.5H29.197C26.9461 72.5 25.2588 70.5713 25.5289 68.4469L27.566 52.4248C27.7933 50.637 29.3491 49.2606 31.2341 49.2606H38.0714C39.2834 50.8179 41.1575 52.0131 43.0696 52.8385C45.3025 53.8026 47.8322 54.3693 50 54.3693Z" stroke="#F2F2F2"
+    stroke-width="3" stroke-linejoin="round"/>
+    <g filter="url(#filter0_d)">
+    <circle
+    cx="50" cy="46"
+    r="44.5" stroke="#F2F2F2"
+    stroke-width="3"/>
+    </g>
+    <defs>
+    <filter
+    id="filter0_d" x="0"
+    y="0" width="100"
+    height="100" filterUnits="userSpaceOnUse"
+    color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix
+    in="SourceAlpha" type="matrix"
+    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+    <feOffset dy="4"/>
+    <feGaussianBlur stdDeviation="2"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+    <feBlend
+    mode="normal" in2="BackgroundImageFix"
+    result="effect1_dropShadow"/>
+    <feBlend
+    mode="normal" in="SourceGraphic"
+    in2="effect1_dropShadow" result="shape"/>
+    </filter>
+    </defs>
+  </svg>
+</template>
